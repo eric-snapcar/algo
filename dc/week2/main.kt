c33c0c334ad92abfe4339d7fe7aac2d376a92445
Eric Hth <eric@snapcar.com>
2018-03-19 14:08:55 +0100

    main.kt val splitCounrt : Long toLong() List<Long>

--- dc/week2/main.kt
+++ dc/week2/main.kt
@@ -5,12 +5,15 @@ import java.io.InputStream
 // kotlinc main.kt -include-runtime -d main.jar
 // java -jar main.jar
 fun main(args : Array<String>) {
-  // val array = getArray()
-  val array = listOf(1, 3, 5, 2,4,6)
-  println(countInversion(array))
+   val array = getArray()
+  // val array = listOf(1, 3, 5, 2,4,6,0)
+  val (count, sortedList) = countInversion(array)
+  println(count)
+  // val (countBis, sortedListBis) = countInversion(sortedList)
+  // println(countBis)
 }
-data class InversionData(val count: Int, val sortedList: List<Int> )
-fun countInversion(array:List<Int>) : InversionData{
+data class InversionData(val count: Long, val sortedList: List<Long> )
+fun countInversion(array:List<Long>) : InversionData{
   var size = array.size
   if (size > 1) {
     val halfSize = array.size/2
@@ -24,9 +27,9 @@ fun countInversion(array:List<Int>) : InversionData{
     return InversionData(0,array)
   }
 }
-fun countInversionMerge(leftArraySorted:List<Int>,rightArraySorted:List<Int>): InversionData {
-  var mergeSorted = mutableListOf<Int>()
-  var splitCount = 0
+fun countInversionMerge(leftArraySorted:List<Long>,rightArraySorted:List<Long>): InversionData {
+  var mergeSorted = mutableListOf<Long>()
+  var splitCount : Long = 0
   var leftArraySorted_ = leftArraySorted.toMutableList()
   var rightArraySorted_ = rightArraySorted.toMutableList()
   while(leftArraySorted_.size > 0 || rightArraySorted_.size > 0){
@@ -50,12 +53,12 @@ fun countInversionMerge(leftArraySorted:List<Int>,rightArraySorted:List<Int>): I
   }
   return InversionData(splitCount,mergeSorted)
 }
-fun getArray():List<Int>{
+fun getArray():List<Long>{
   val reader = File("data.txt").inputStream().bufferedReader()
   val iterator = reader.lines().iterator()
-  var list = mutableListOf<Int>()
+  var list = mutableListOf<Long>()
   while(iterator.hasNext()) {
-    list.add(iterator.next().toInt())
+    list.add(iterator.next().toLong())
   }
   reader.close()
   return list
