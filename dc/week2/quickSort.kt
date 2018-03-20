c0a7094843422729968242535446aa37e9aa63cd
Eric Hth <eric@snapcar.com>
2018-03-21 00:02:59 +0100

    quickSort() partition() ... listOf()

---
+++ dc/week2/quickSort.kt
@@ -0,0 +1,57 @@
+
+import java.io.File
+import java.io.InputStream
+// kotlinc quickSort.kt -include-runtime -d quickSort.jar
+// java -jar quickSort.jar
+fun main(args : Array<String>) {
+  val array = getArray()
+  // println(quickSort(array))
+}
+fun quickSort(array:List<Int>) : List<Int>{
+  var size = array.size
+  if (size > 1) {
+    val pivotIndex =  size/2
+    val (pivotValue, leftArray, rightArray) = partition(pivotIndex,array)
+    return quickSort(leftArray) + listOf(pivotValue) + quickSort(rightArray)
+  }else {
+    return array
+  }
+}
+data class PartitionData(val pivotValue: Int, val leftArray: List<Int>, val rightArray: List<Int> )
+fun partition(pivotIndex : Int,array : List<Int>): PartitionData {
+  return PartitionData(1,array,array)
+}
+fun merge(leftArraySorted:List<Int>,rightArraySorted:List<Int>): List<Int>{
+  var mergeSorted = mutableListOf<Int>()
+  var leftArraySorted_ = leftArraySorted.toMutableList()
+  var rightArraySorted_ = rightArraySorted.toMutableList()
+  while(leftArraySorted_.size > 0 || rightArraySorted_.size > 0){
+    if(leftArraySorted_.size == 0 ){
+      mergeSorted.add(rightArraySorted_.first())
+      rightArraySorted_.removeAt(0)
+    }else if(rightArraySorted_.size == 0){
+      mergeSorted.add(leftArraySorted_.first())
+      leftArraySorted_.removeAt(0)
+    }
+    else {
+      if(leftArraySorted_.first() < rightArraySorted_.first()){
+        mergeSorted.add(leftArraySorted_.first())
+        leftArraySorted_.removeAt(0)
+      }else {
+        mergeSorted.add(rightArraySorted_.first())
+        rightArraySorted_.removeAt(0)
+      }
+    }
+  }
+  return mergeSorted
+}
+fun getArray():List<Int>{
+  val reader = File("data.txt").inputStream().bufferedReader()
+  val iterator = reader.lines().iterator()
+  var list = mutableListOf<Int>()
+  while(iterator.hasNext()) {
+    list.add(iterator.next().toInt())
+  }
+  reader.close()
+  return list
+}
